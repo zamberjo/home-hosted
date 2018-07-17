a2d1c655a2f100122e497b8f166c824a4fda076f
zambu <zambu@zambu.zambu.local>
2018-07-17 13:17:34 +0200

    [MOD] rtorrent build

--- rtorrent/rootfs/usr/local/bin/run.sh
+++ rtorrent/rootfs/usr/local/bin/run.sh
@@ -6,7 +6,7 @@ mkdir -p /data/.session
 
 rm -f /data/.session/rtorrent.lock
 
-chown -R $UID:$GID /data /home/torrent /tmp /usr/flood /flood-db /etc/s6.d
+# chown -R $UID:$GID /data /home/torrent /tmp /usr/flood /flood-db /etc/s6.d
 
 if [ ${RTORRENT_SCGI} -ne 0 ]; then
     sed -i -e 's|^scgi_local.*$|scgi_port = 0.0.0.0:'${RTORRENT_SCGI}'|' /home/torrent/.rtorrent.rc
