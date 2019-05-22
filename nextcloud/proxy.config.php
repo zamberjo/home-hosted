7c2f2cd14160b515712e57d4c564218aa6078c39
Jose Zambudio Bernabeu <zamberjo@gmail.com>
2019-05-22 17:24:02 +0200

    .

--- nextcloud/proxy.config.php
+++ nextcloud/proxy.config.php
@@ -1,9 +1,10 @@
 <?php
 $CONFIG = array(
-  'trusted_proxies' => array(
-    gethostbyname('proxy'),
-  ),
-  'forwarded_for_headers' => array('HTTP_X_FORWARDED_FOR'),
+  // 'trusted_proxies' => array(
+  //   gethostbyname('proxy'),
+  // ),
+  // 'forwarded_for_headers' => array('HTTP_X_FORWARDED_FOR'),
   'overwrite.cli.url' => 'https://nextcloud.josezambudiobernabeu.com/',
   'overwriteprotocol' => 'https',
+  'csrf.disabled' => true,
 );
