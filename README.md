60e95b024ce7a7521be049f64a19f0fbf4d63a56
Jose Zambudio Bernabeu <zamberjo@gmail.com>
2019-05-25 09:44:58 +0200

    [ADD] script init/build stacks

--- README.md
+++ README.md
@@ -5,17 +5,21 @@ docker network create postgres-network
 docker network create redis-network
 ```
 
+
 ## Deploy PostgreSQL
 - `docker-compose -f stack-postgres.yml -p postgres up -d`
 
+
 ## Deploy Redis
 - `docker-compose -f stack-redis.yml -p redis up -d`
+
 
 ## Deploy Traefik
 - `docker-compose -f stack-proxy.yml -p proxy up -d`
 - *DEVEL*: `docker-compose -f stack-proxy.yml -f stack-proxy.devel.yml -p proxy up -d`
 
-## Deploy rTorrent + Flood
+
+## Deploy rTorrent + Flood + couchpotato + jackett
 - `mkdir -pv $HOME/{downloads,watch,incoming,couchpotato}`
 - `mkdir -pv $HOME/jackett/{config,blackhole}`
 - `docker volume create --name rtorrent-incoming-volume -d local -o o=bind -o type=none -o device=$HOME/incoming`
@@ -25,30 +29,35 @@ docker network create redis-network
 - `docker volume create --name jackett-blackhole-volume -d local -o o=bind -o type=none -o device=$HOME/jackett/blackhole`
 - `docker volume create --name jackett-config-volume -d local -o o=bind -o type=none -o device=$HOME/jackett/config`
 - `docker-compose -f stack-rtorrent.yml -p rtorrent up -d`
-- *ARM*: `docker-compose -f stack-rtorrent.yml -f stack-rtorrent.armhf.yml -p rtorrent up -d`
+- *ARM*: `docker-compose -f stack-rtorrent.yml -f stack-rtorrent.armv8.yml -p rtorrent up -d`
 - *DEVEL*: `docker-compose -f stack-rtorrent.yml -f stack-rtorrent.devel.yml -p rtorrent up -d`
 - *USE*: `tvshows` and `movies` tags
+
 
 ## Deploy Plex
 - `mkdir -pv $HOME/plex/config`
 - `docker volume create --name plex-config-volume -d local -o o=bind -o type=none -o device=$HOME/plex/config`
 - `docker-compose -f stack-plex.yml -p plex up -d`
-- *ARM*: `docker-compose -f stack-plex.yml -f stack-plex.armhf.yml -p plex up -d`
+- *ARM*: `docker-compose -f stack-plex.yml -f stack-plex.armv8.yml -p plex up -d`
 - *DEVEL*: `docker-compose -f stack-plex.yml -f stack-plex.devel.yml -p plex up -d`
+
 
 ## Deploy HomeAssistant
 - `mkdir -pv $HOME/homeassistant-config`
 - `docker volume create --name homeassistant-config-volume -d local -o o=bind -o type=none -o device=$HOME/homeassistant-config`
 - `docker-compose -f stack-homeassistant.yml -p homeassistant up -d`
-- *ARM*: `docker-compose -f stack-homeassistant.yml -f stack-homeassistant.armhf.yml -p homeassistant up -d`
+- *ARM*: `docker-compose -f stack-homeassistant.yml -f stack-homeassistant.armv8.yml -p homeassistant up -d`
 - *DEVEL*: `docker-compose -f stack-homeassistant.yml -f stack-homeassistant.devel.yml -p homeassistant up -d`
+
 
 ## Prometheus
 - `docker-compose -f stack-prometheus.yml -p prometheus up -d`
-- *ARM*: `docker-compose -f stack-prometheus.yml -f stack-prometheus.armhf.yml -p prometheus up -d`
+- *ARM*: `docker-compose -f stack-prometheus.yml -f stack-prometheus.armv8.yml -p prometheus up -d`
+
 
 ## Grafana
 - `docker-compose -f stack-grafana.yml -p grafana up -d`
+
 
 ## Deploy NextCloud
 - `docker-compose -f stack-nextcloud.yml -p nextcloud up -d`
---
+++ stack-armv8.sh
@@ -0,0 +1,191 @@
+#!/bin/sh
+
+docker inspect proxy-network &> /dev/null || docker network create proxy-network
+docker inspect postgres-network &> /dev/null || docker network create postgres-network
+docker inspect redis-network &> /dev/null || docker network create redis-network
+
+ARG_PG=0
+ARG_REDIS=0
+ARG_TRAEFIK=0
+ARG_RTORRENT=0
+ARG_PLEX=0
+ARG_HASS=0
+ARG_PROMETHEUS=0
+ARG_GRAFANA=0
+ARG_NEXTCLOUD=0
+ARG_ALL=0
+
+print_help () {
+    >&2 cat <<EOF
+Usage:
+  ./stack-arm64 [ARGS]
+Arguments:
+  --pg            Force build PostgreSQL stack.
+  --redis         Force build Redis stack.
+  --traefik       Force build Traefik stack.
+  --rtorrent      Force build Rtorrent + Flood + Couchpotato + Jckett stack.
+  --plex          Force build Plex stack.
+  --hass          Force build Homeassistant stack.
+  --prometheus    Force build Prometheus stack.
+  --grafana       Force build Grafana stack.
+  --nextcloud     Force build Nextcloud stack.
+  --all           Force build all stacks.
+Examples:
+  ./stack-arm64 --all
+  ./stack-arm64 --traefik
+  ./stack-arm64 --prometheus --grafana
+  ./stack-arm64 --nextcloud --pg --redis
+EOF
+    exit 1
+}
+
+while [[ ${#} -gt 0 ]]; do
+    case "${1}" in
+        --pg)
+            ARG_PG=1
+            ;;
+        --redis)
+            ARG_REDIS=1
+            ;;
+        --traefik)
+            ARG_TRAEFIK=1
+            ;;
+        --rtorrent)
+            ARG_RTORRENT=1
+            ;;
+        --plex)
+            ARG_PLEX=1
+            ;;
+        --hass)
+            ARG_HASS=1
+            ;;
+        --prometheus)
+            ARG_PROMETHEUS=1
+            ;;
+        --grafana)
+            ARG_GRAFANA=1
+            ;;
+        --nextcloud)
+            ARG_NEXTCLOUD=1
+            ;;
+        --all)
+            ARG_ALL=1
+            ;;
+        *)
+            >&2 echo "[ERROR] unknown arg: ${1}"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+if [[ ${ARG_PG} -eq 1 ]] || [[ ${ARG_ALL} -eq 1 ]]; then
+    docker-compose \
+        -f stack-postgres.yml \
+        -p postgres \
+        up -d --build --force-recreate
+fi
+
+if [[ ${ARG_REDIS} -eq 1 ]] || [[ ${ARG_ALL} -eq 1 ]]; then
+    docker-compose \
+        -f stack-redis.yml \
+        -p redis \
+        up -d --build --force-recreate
+fi
+
+if [[ ${ARG_TRAEFIK} -eq 1 ]] || [[ ${ARG_ALL} -eq 1 ]]; then
+    docker-compose \
+        -f stack-proxy.yml \
+        -p proxy \
+        up -d --build --force-recreate
+fi
+
+if [[ ${ARG_RTORRENT} -eq 1 ]] || [[ ${ARG_ALL} -eq 1 ]]; then
+    docker inspect rtorrent-incoming-volume &> /dev/null
+    if [[ $? -eq 1 ]]; then
+        >&2 echo "[ERROR] Before build, you must create rtorrent-incoming-volume volume!"
+        exit 1
+    fi
+    docker inspect rtorrent-downloads-volume &> /dev/null
+    if [[ $? -eq 1 ]]; then
+        >&2 echo "[ERROR] Before build, you must create rtorrent-downloads-volume volume!"
+        exit 1
+    fi
+    docker inspect rtorrent-watch-volume &> /dev/null
+    if [[ $? -eq 1 ]]; then
+        >&2 echo "[ERROR] Before build, you must create rtorrent-watch-volume volume!"
+        exit 1
+    fi
+    docker inspect couchpotato-volume &> /dev/null
+    if [[ $? -eq 1 ]]; then
+        >&2 echo "[ERROR] Before build, you must create couchpotato-volume volume!"
+        exit 1
+    fi
+    docker inspect jackett-blackhole-volume &> /dev/null
+    if [[ $? -eq 1 ]]; then
+        >&2 echo "[ERROR] Before build, you must create jackett-blackhole-volume volume!"
+        exit 1
+    fi
+    docker inspect jackett-config-volume &> /dev/null
+    if [[ $? -eq 1 ]]; then
+        >&2 echo "[ERROR] Before build, you must create jackett-config-volume volume!"
+        exit 1
+    fi
+    docker-compose \
+        -f stack-rtorrent.yml \
+        -f stack-rtorrent.armv8.yml \
+        -p rtorrent \
+        up -d --build --force-recreate
+fi
+
+if [[ ${ARG_PLEX} -eq 1 ]] || [[ ${ARG_ALL} -eq 1 ]]; then
+    docker inspect plex-config-volume &> /dev/null
+    if [[ $? -eq 1 ]]; then
+        >&2 echo "[ERROR] Before build, you must create plex-config-volume volume!"
+        exit 1
+    fi
+    docker-compose \
+        -f stack-plex.yml \
+        -f stack-plex.armv8.yml \
+        -p plex \
+        up -d --build --force-recreate
+fi
+
+if [[ ${ARG_HASS} -eq 1 ]] || [[ ${ARG_ALL} -eq 1 ]]; then
+    docker inspect homeassistant-config-volume &> /dev/null
+    if [[ $? -eq 1 ]]; then
+        >&2 echo "[ERROR] Before build, you must create homeassistant-config-volume volume!"
+        exit 1
+    fi
+    docker-compose \
+        -f stack-homeassistant.yml \
+        -f stack-homeassistant.armv8.yml \
+        -p homeassistant \
+        up -d --build --force-recreate
+fi
+
+if [[ ${ARG_PROMETHEUS} -eq 1 ]] || [[ ${ARG_ALL} -eq 1 ]]; then
+    docker-compose \
+        -f stack-prometheus.yml \
+        -f stack-prometheus.armv8.yml \
+        -p prometheus \
+        up -d --build --force-recreate
+fi
+
+if [[ ${ARG_GRAFANA} -eq 1 ]] || [[ ${ARG_ALL} -eq 1 ]]; then
+    docker-compose \
+        -f stack-grafana.yml \
+        -p grafana \
+        up -d --build --force-recreate
+fi
+
+if [[ ${ARG_NEXTCLOUD} -eq 1 ]] || [[ ${ARG_ALL} -eq 1 ]]; then
+    docker-compose \
+        -f stack-nextcloud.yml \
+        -p nextcloud \
+        up -d --build --force-recreate
+    docker-compose \
+        -f stack-nextcloud-cron.yml \
+        -p nextcloud_cron \
+        up -d --build --force-recreate
+fi
